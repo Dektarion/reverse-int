ae848a9ef99e4d7880d8c762551c70d1d65809be
Dektarion <andreiy.boyarsky@gmail.com>
2022-06-24 20:41:11 +0400

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  let nString = n.toString();
+  let i = 0;
+  let result = "";
+
+  while (i < nString.length) {
+    result = `${nString[i]}${result}`;
+    i++;
+  }
+  return Math.abs(parseInt(result));
+};
